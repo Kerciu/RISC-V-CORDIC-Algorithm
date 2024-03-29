19fb7c8f871f854068f750d6bc896f90dfdf6f66
Kerciu <kerciuuu@gmail.com>
2024-03-29 15:08:42 +0100

    semi-working CORDIC in python

--- CORDIC-Python/CORDIC.py
+++ CORDIC-Python/CORDIC.py
@@ -1,33 +1,60 @@
 # Python implementation for cordic algorithm
-
-def CORDIC(theta, N):
-    x = 0               # Wartość początkowa sinus
-    y = 0x9B        # Wartość początkowa cosinus  / /  155
-    angle = theta       # Kąt
-
-    acan_table = [61, 45, 24, 12, 6, 3, 2, 1]
-    # acan_table = [0x3FDFFFFFFF993, 0x3FDFFFFFE36F, 0x3FDFFFFF1C76,
-    #               0x3FDFFFFDDEDD, 0x3FDFFFFA26C4, 0x3FDFFFF40F3B,
-    #               0x3FDFFFE03823, 0x3FDFFFBBBD2E]
-
-    for i in range(N):
-        if angle < 0:
-            new_x = x + (y >> i)       # Przesuwamy o i miejsc bitowych w prawo
-            new_y = y - (x >> i)
-            angle += acan_table[i % len(acan_table)]
-        else:
-            new_x = x - (y >> i)
-            new_y = y + (x >> i)
-            angle -= acan_table[i % len(acan_table)]
-
-        x, y = new_x, new_y
-
-    return x, y
+from math import atan, sin, cos, pi, sqrt
 
 
-# Decide precision level
-N = 15
-# Input angle
-theta = 50
-cordic = CORDIC(theta, N)
-print(f"Sin({theta}): {cordic[0]}, Cos({theta}): {cordic[1]}")
+def generate_atans():
+    atan_scale = 0x40000000
+    array = []
+    for i in range(20):
+        array.append(int(atan(2 ** - i) * atan_scale))
+
+    return array
+
+
+def hexadecimate_array(array):
+    for idx, elem in enumerate(array):
+        array[idx] = hex(elem)
+    return array
+
+
+def K_value(iterations):
+    K = 1.0
+    for i in range(0, iterations):
+        K = K * (1.0 / sqrt(1 + 2.0 ** (-2 * i)))
+    return K
+
+
+def CORDIC(theta_deg, iterations):
+    xy_point = (1, 0)
+    K = K_value(iterations)
+
+    for i in range(0, iterations):
+
+        d = 1.0
+        if theta_deg < 0:
+            d = -1.0
+
+        x, y = xy_point[0], xy_point[1]
+
+        xy_point = (x - (d * (2.0 ** (-i))) * y, y + (d * (2.0 ** (-i))))
+        theta_deg = theta_deg - (d * atan(2 ** (-i)))
+
+    return K * xy_point[0], K * xy_point[1]
+
+
+if __name__ == "__main__":
+    scale = 0x26DD3B6A
+    table = generate_atans()
+    len_of_array = len(table)
+    cordic_K = 0x100000000
+
+    angle = pi / 2
+    actual_sin = sin(angle)
+    actual_cos = cos(angle)
+    cordic_cos, cordic_sin = CORDIC(angle, len_of_array)
+
+    print("Angle:", angle)
+    print("Actual Sin:", actual_sin)
+    print("CORDIC Sin:", cordic_sin)
+    print("Actual Cos:", actual_cos)
+    print("CORDIC Cos:", cordic_cos)
