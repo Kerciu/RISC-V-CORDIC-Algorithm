f8cefb86fc5aa0a52272e368a2beae55cf508655
Kerciu <kerciuuu@gmail.com>
2024-03-31 15:56:32 +0200

    done C implementation of cordic algorithm

--- CORDIC-C/CORDIC.c
+++ CORDIC-C/CORDIC.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 
-int* CORDIC(int ACAN_TABLE[], int acan_size, int theta_deg) {
+void CORDIC(int ACAN_TABLE[], int acan_size, int theta_deg, int* result) {
     int K = 0x2431f1c7;
     int x_point = K;
     int y_point = 0;
@@ -23,11 +23,18 @@ int* CORDIC(int ACAN_TABLE[], int acan_size, int theta_deg) {
             z -= dz;
         }
 
-    int result[2] = {x_point, y_point};
-
-    return result;
+    result[0] = x_point;
+    result[1] = y_point;
     }
 }
+
+int power_of(int degree) {
+    for (int i = 0; i < degree; i++) {
+        degree *= degree;
+    }
+    return degree;
+}
+
 
 int main() {
 
@@ -36,13 +43,17 @@ int main() {
         0xfffff, 0x7ffff, 0x3ffff, 0x1ffff, 0xffff, 0x7fff, 0x3fff,
         0x1fff, 0xfff, 0x7ff, 0x3ff, 0x1ff, 0xff, 0x7f, 0x3f, 0x1f,
 	    0xf, 0x8, 0x4, 0x2, 0x1, 0x0};
-
     int size = sizeof(ACAN_TABLE) / sizeof(ACAN_TABLE[0]);
 
-    float angle = (3141592653 >> 9) / 2;
-    int cordic_result[1] = CORDIC(ACAN_TABLE, size, angle)
 
-    printf("Sine: %r", cordic_result[1], '\n');
-    printf("Cosine: %r", cordic_result[0]);
+    int cordic_result[2];
+    float angle = (3141592653) / 3;
+    CORDIC(ACAN_TABLE, size, angle, cordic_result);
+
+    int sine = cordic_result[1];
+    int cosine = cordic_result[0];
+
+    printf("Sine: %d", sine, '\n');
+    printf(" Cosine: %d", cosine);
     return 0;
 }
