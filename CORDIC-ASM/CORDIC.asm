74f3d75a8d76175b08b0cd02b4fb3d64992b5cf0
Kerciu <kerciuuu@gmail.com>
2024-04-13 20:22:53 +0200

    added sine and cosine .asciz

--- CORDIC-ASM/CORDIC.asm
+++ CORDIC-ASM/CORDIC.asm
@@ -3,6 +3,8 @@
 	.data
 prompt: .asciz "Enter angle in: "
 prompt_error: .asciz "You have provided wrong input"
+sine_output: .asciz "\nSine: "
+cosine_output: .asciz "\nCosine: "
 
 ACAN_TABLE: .word 0x3243f6a8, 0x1dac6705, 0x0fadbafc, 0x07f56ea6, 0x03feab76, 0x01ffd55b, 0x00fffaaa, 0x007fff55, 0x003fffea, 0x001ffffd, 0x000fffff, 0x0007ffff, 0x0003ffff, 0x0001ffff, 0x0000ffff, 0x00007fff, 0x00003fff, 0x00001fff, 0x00000fff, 0x000007ff, 0x000003ff, 0x000001ff, 0x000000ff, 0x0000007f, 0x0000003f, 0x0000001f, 0x0000000f, 0x00000008, 0x00000004, 0x00000002, 0x00000001, 0x00000000
 K: .word 0x26DD3B6A
@@ -58,7 +60,23 @@ increment_pointer:
 	beq t3, s4, end
 	j acan_table_iteration
 
-end:
+end:	
+	li a7, 4
+	la a0, sine_output
+	ecall
+	
+	li a7, 1
+	mv a0, t1
+	ecall
+	
+	li a7, 4
+	la a0, cosine_output
+	ecall
+	
+	li a7, 1
+	mv a0, t0
+	ecall
+	
 	li  a7, 10
 	ecall
 
