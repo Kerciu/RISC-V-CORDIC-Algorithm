b62def8b813cde1866fef7b42c86e09c71924a47
Kerciu <kerciuuu@gmail.com>
2024-04-13 14:21:10 +0200

    fixed bugs

--- CORDIC-Python/CORDIC.py
+++ CORDIC-Python/CORDIC.py
@@ -16,7 +16,7 @@ def generate_atans():
     """
     atan_scale = 0x40000000
     array = []
-    for i in range(20):
+    for i in range(32):
         array.append(int(atan(2 ** - i) * atan_scale))
 
     return array
@@ -28,7 +28,7 @@ def hexadecimate_array(array):
     """
     # This function will be used for .asm program to generate array
     for idx, elem in enumerate(array):
-        array[idx] = hex(elem)
+        array[idx] = f"{elem:#0{10}x}"
     return array
 
 
@@ -55,38 +55,41 @@ def K_value(iterations):
     return K
 
 
-def CORDIC(theta_deg, iterations):
+def SCALE():
+    return 2**30
+
+
+def CORDIC(theta_deg, ACAN_TABLE, iterations):
     """
     Cordic Algorithm:
     Find cosine and sine through iterations
     :param theta_deg: Angle in degrees
+    :param ACAN_TABLE: Table of precomputed arctangent
     :param iterations: Number of iterations
     :return: Tuple of cosine and sine values
     """
     # Initial point on a plot
-    xy_point = (1, 0)
+    K = int(K_value(32) * SCALE())
+    x, y, z = int(K), 0, theta_deg
 
     # Fetch scaling factor
-    K = K_value(iterations)
 
-    for i in range(0, iterations):
+    for i in range(iterations):
 
-        # Determine rotation based on an angle sign
-        d = 1.0
-        if theta_deg < 0:
-            d = -1.0
+        dx, dy, dz = y >> i, x >> i, ACAN_TABLE[i]
+        # Determine rotation based on an angle sig
+        if z < 0:
+            x += dx
+            y -= dy
+            z += dz
 
-        # Extract x and y coordinates
-        x, y = xy_point[0], xy_point[1]
-
-        # Update coordinates using CORDIC algorithm equations
-        xy_point = (x - (d * (2.0 ** (-i))) * y, y + (d * (2.0 ** (-i))))
-
-        # Update angle
-        theta_deg = theta_deg - (d * atan(2 ** (-i)))
+        else:
+            x -= dx
+            y += dy
+            z -= dz
 
     # Scale and return cosine and sine values
-    return K * xy_point[0], K * xy_point[1]
+    return {"Sine": y, "Cosine": x}
 
 
 #  ------------------------------------------
@@ -99,17 +102,31 @@ if __name__ == "__main__":
 
     table = generate_atans()
     len_of_array = len(table)
+    scale = SCALE()
 
     angle = pi / 6
+    fixed_angle = angle * scale
     actual_sin = sin(angle)
     actual_cos = cos(angle)
-    cordic_cos, cordic_sin = CORDIC(angle, len_of_array)
+    cordic_dict = CORDIC(fixed_angle, table, len_of_array)
 
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
     print("Angle:", angle)
     print("Actual Sin:", actual_sin)
-    print("CORDIC Sin:", cordic_sin)
+    print("CORDIC Sin:", cordic_dict["Sine"] / scale)
     print("Actual Cos:", actual_cos)
-    print("CORDIC Cos:", cordic_cos)
-
-    print(hexadecimate_array(generate_atans()))
-    print(hex(convert_to_int(K_value(32))))
+    print("CORDIC Cos:", cordic_dict["Cosine"] / scale)
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
+    Scaled_K = int(K_value(32) * SCALE())
+    print("Arctangent table in int: ", generate_atans())
+    print("Arctangent table in hex: ", hexadecimate_array(generate_atans()))
+    print("Hex value of K constant: ", hex(convert_to_int(K_value(32))))
+    print("Scale K constant: ", Scaled_K, " == ", hex(Scaled_K))
+    print("Scale: ", SCALE())
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
+    print("--------------------------------------------------------")
